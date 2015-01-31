b80d1b7a5578c3b41f5b05a799f3a496f690c874
Arcadiy Ivanov <arcadiy.ivanov@servicemesh.com>
2015-01-31 02:14:24 -0500

    Remove unused imports

--- src/main/java/org/wildfly/extension/picketlink/federation/model/keystore/KeyAddHandler.java
+++ src/main/java/org/wildfly/extension/picketlink/federation/model/keystore/KeyAddHandler.java
@@ -28,7 +28,6 @@ import org.jboss.as.controller.OperationFailedException;
 import org.jboss.as.controller.PathAddress;
 import org.jboss.as.controller.ServiceVerificationHandler;
 import org.jboss.as.controller.SimpleAttributeDefinition;
-import org.jboss.as.controller.registry.Resource;
 import org.jboss.dmr.ModelNode;
 import org.jboss.msc.service.ServiceBuilder;
 import org.jboss.msc.service.ServiceController;
